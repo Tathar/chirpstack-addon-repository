053ce62822047bbbe252e504b16169eecb4e94f3
Tathar <tathar.dev@gmail.com>
2024-03-28 15:40:31 +0100

    Update run.sh

--- network-server/scripts/run.sh
+++ network-server/scripts/run.sh
@@ -4,14 +4,14 @@
 # This script have to be duplicated in all the services, because Docker doesn't support
 # copying files into container from outside the current working directory.
 
-SERVICE=chirpstack-network-server
+SERVICE=chirpstack
 
 # Update config based on add-on config
 /scripts/update_config.sh
 
 # Start the Chirpstack service
 bashio::log.info "Starting $SERVICE..."
-/etc/init.d/$SERVICE start
+systemctl start $SERVICE
 
 # Keep container running
 tail -F /var/log/$SERVICE/$SERVICE.log
